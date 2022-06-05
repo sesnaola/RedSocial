2abaa9569e2bee8da4d8f37ccaabf1d31b77a379
Sergi Esnaola <sergiesnaola@gmail.com>
2022-06-05 14:43:43 +0200

    Added authentication method.
    How it works:
    
    - call "llocalhost:3080/login"
    - use x-www-form-urlencoded" body type
    - enter "mail" and "password"
    - if correct, a JWT token is delivered. Use it to make any other call to the API. (authorization header)
    
    JWT validation not implemented yet for the other API calls.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+
 const routes = require('./routes/routes');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.json());
 app.use('/', routes);
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,8 +1,10 @@
 const express = require('express'); //import express
 const router = express.Router();
 const usersController = require('./../controllers/UsersControllers');
+const authController = require('./../controllers/AuthController');
 
 router.get('/users', usersController.getAllUsers);
 router.get('/users/:id', usersController.getUser);
+router.post('/login', authController.login);
 
 module.exports = router;
