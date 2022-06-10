e0ab4ef22f98c8047bf3348096c581ffb07ab613
Joan Pérez <joan.perezsanchez@gmail.com>
2022-06-10 20:03:34 +0200

    registration form with logic

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/login">Login</router-link>
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/register">Register</router-link>
     <router-view />
   </nav>
 </template>
---
+++ client/src/views/RegisterView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="register">
+    <RegisterItem />
+  </div>
+</template>
+
+<script>
+import RegisterItem from "@/components/RegisterItem.vue";
+
+export default {
+  name: "RegisterView",
+  components: {
+    RegisterItem,
+  },
+};
+</script>
+
+<style></style>
