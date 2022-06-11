f692c29a01c63988ae7d367930a0ce3421071504
Joan Pérez <joan.perezsanchez@gmail.com>
2022-06-11 15:57:46 +0200

    Created the posts view. Retrieve the information from the API. Displaying the data iteratively in the view.

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,8 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/login">Login</router-link> |
-    <router-link to="/register">Register</router-link>
+    <router-link to="/register">Register</router-link> |
+    <router-link to="/posts">Posts</router-link>
     <router-view />
   </nav>
 </template>
---
+++ client/src/views/PostsView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="posts">
+    <div class="posts-header">
+      <h2>Posts</h2>
+    </div>
+    <PostItem v-for="post in posts" :key="post.id" :post="post" />
+  </div>
+</template>
+
+<script>
+import socialnetwork from "@/services/socialnetwork";
+import PostItem from "@/components/PostItem.vue";
+export default {
+  data() {
+    return {
+      posts: null,
+    };
+  },
+  components: {
+    PostItem,
+  },
+  async created() {
+    await this.getPosts();
+  },
+  methods: {
+    async getPosts() {
+      await socialnetwork.getPosts().then((res) => {
+        this.posts = res.data.posts;
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
